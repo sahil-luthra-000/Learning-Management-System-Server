92f529c078ae90aa72de07c824e7d8112be92a72
Sahil <sahilluthra495@gmail.com>
2025-05-12 23:02:22 +0530

    changes

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/uploads", express.static("uploads"));
+app.use(express.static('public'));
 
 // importing routes
 import userRoutes from "./routes/user.js";
